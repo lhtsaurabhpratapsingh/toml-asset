f9ce90b6cacfb1195178f5835e0134ebd07c9c66
lhtsaurabhsingh <saurabhp@leewayhertz.com>
2022-06-07 12:48:10 +0530

    stellar

--- index.js
+++ index.js
@@ -10,39 +10,39 @@ const app = express();
 const PORT =  5000;
 
 app.use(cors());
-app.get('/.well-known/stellar.toml', (req, res, next) => {
-    const options = {
-      root: path.join(__dirname, 'public'),
-    }
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("content-type", "text/plain");
-    res.sendFile('stellar.toml', options);
-  })
+// app.get('/.well-known/stellar.toml', (req, res, next) => {
+//     const options = {
+//       root: path.join(__dirname, 'public'),
+//     }
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     res.header("content-type", "text/plain");
+//     res.sendFile('stellar.toml', options);
+//   })
 
-  app.get('/sep24/info',(req,res)=>{
-    res.json({
-        "deposit": {
-            "stellarTOML": {
-                "enabled": true,
-                "fee_fixed": 1.0
-            }
-        },
-        "withdraw": {
-            "stellarTOML": {
-                "enabled": true,
-                "fee_fixed": 1.0
-            }
-        },
-        "fee": {
-            "enabled": true
-        },
-        "features": {
-            "account_creation": true,
-            "claimable_balances": true
-        }
-    })
-    })
+//   app.get('/sep24/info',(req,res)=>{
+//     res.json({
+//         "deposit": {
+//             "stellarTOML": {
+//                 "enabled": true,
+//                 "fee_fixed": 1.0
+//             }
+//         },
+//         "withdraw": {
+//             "stellarTOML": {
+//                 "enabled": true,
+//                 "fee_fixed": 1.0
+//             }
+//         },
+//         "fee": {
+//             "enabled": true
+//         },
+//         "features": {
+//             "account_creation": true,
+//             "claimable_balances": true
+//         }
+//     })
+//     })
     // endpoint
 
     // app.get('/auth', (req,res)=>{
